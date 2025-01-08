8a193d64a25ce2a4e013549e939059bbbf1d20b2
Mendrika <mendrikarkt18@gmail.com>
2025-01-08 16:04:27 +0300

    add chapter about variables and data types

--- variables_and_data_types/bin/example1.dart
+++ variables_and_data_types/bin/example1.dart
@@ -5,6 +5,7 @@ void main(List<String> args) {
   var location = 'Madagascar';
   bool isMarried = false;
   
+  // ignore: dead_code
   print('My name is $firstName $lastName and I\'m $age years old and I live in $location and I am ${isMarried ? 'married' : 'not married'}');
 
   // firstName = 'Michael' ; // Error: Constant variables can't be reassigned a value.
